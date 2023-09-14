6a8facfe60fd6640475481f62941141e7f6d65d0
nadinespy <nadine.spychala@gmail.com>
2023-09-14 20:16:58 +0100

    reword notebook, relative paths

--- src/global_food_shocks.py
+++ src/global_food_shocks.py
@@ -4,26 +4,6 @@ import numpy as np
 import pandas as pd
 
 
-def rename_columns(df, rename_dict):
-    """
-    Renames columns of a DataFrame based on a provided dictionary.
-
-    Args:
-    - df (pd.DataFrame): The DataFrame whose columns are to be renamed.
-    - rename_dict (dict): A dictionary where keys are the old column names and values are the new column names.
-
-    Returns:
-    - pd.DataFrame: DataFrame with renamed columns.
-    """
-    new_column_names = [rename_dict[col] if col in rename_dict else col for col in df.columns]
-
-    if len(new_column_names) != len(set(new_column_names)):
-        raise ValueError("Renaming would create duplicate column names")
-
-    return df.rename(columns=rename_dict, inplace=True)
-
-
-
 def replace_values_in_columns(df, columns, values_to_replace, replacement=np.nan):
     """
     Replaces specified values in given DataFrame columns.
@@ -88,7 +68,8 @@ def calc_miss_val_percentages(df, variables_to_compute, crops, grouping_variable
     return result_df
 
 
-def plot_miss_val_percentages(df, variables_to_compute, crops, grouping_variables, pathout):
+
+def plot_miss_val_percentages(df, variables_to_compute, crops, grouping_variables, pathout=None):
     """
     Plot missing data percentages for all specified variables from the given DataFrame.
 
@@ -98,7 +79,8 @@ def plot_miss_val_percentages(df, variables_to_compute, crops, grouping_variable
     - variables_to_compute: List of column names whose missing data percentage needs to be plotted.
     - crops: List of crops for which the missing data percentages are to be plotted.
     - grouping_variables: List of columns by which data is grouped.
-    - pathout: Output path for saving the plots (without file extension).
+    - pathout (optional): Output path for saving the plots (without file extension). If not provided or set to None,
+      plots will not be saved.
     """
     # filter the dataframe for only the crops in question
     df = df[df['crop'].isin(crops)]
@@ -112,12 +94,15 @@ def plot_miss_val_percentages(df, variables_to_compute, crops, grouping_variable
     
     for variable in variables_to_compute:
         plt.figure(figsize=(12, 8))
-        sns.barplot(x=x_label, y=variable, hue='crop', data=df, ci=None)
+        sns.barplot(x=x_label, y=variable, hue='crop', data=df, errorbar=None)
         plt.title(f"missing data percentage for {variable} grouped by {', '.join(grouping_variables)}")
         plt.ylabel("missing percentage (%)")
         plt.xticks(rotation=45)
         plt.tight_layout()
-        plt.savefig(pathout + f'distr_{variable}_by_{"_".join(grouping_variables)}.png', bbox_inches='tight', dpi=300)
+        
+        if pathout is not None:
+            plt.savefig(pathout + f'distr_{variable}_by_{"_".join(grouping_variables)}.png', bbox_inches='tight', dpi=300)
+        
         plt.show()
         plt.close()
 
@@ -166,10 +151,10 @@ def plot_histograms(df, plot_var, grouping_var, crops, num_rows, pathout_plots):
         # create a histogram on the current subplot, color-coded for each crop
         for crop in crops:
             sns.histplot(data=data_for_value[data_for_value['crop'] == crop], x=plot_var, ax=axes[i], kde=True, label=crop)
-            axes[i].legend()
-            axes[i].set_title(f'{value}')
-            axes[i].set_xlabel(plot_var)
-            axes[i].set_ylabel('density')
+        axes[i].legend()
+        axes[i].set_title(f'{value}')
+        axes[i].set_xlabel(plot_var)
+        axes[i].set_ylabel('density')
 
     # remove any remaining empty subplots
     for j in range(len(unique_values), len(axes)):
@@ -210,11 +195,11 @@ def plot_time_series_by_group(df, y_var, pathout_plots, crops, group_var=None, m
     # if no grouping is required
     if group_var is None:
         plt.figure(figsize=(10, 6))
-        sns.lineplot(data=df, x="year", y=y_var, hue='crop', ci=95, estimator=metric)
+        sns.lineplot(data=df, x="year", y=y_var, hue='crop', errorbar=('ci', 95), estimator=metric)
         plt.title(f'time series of {metric} of {y_var}', fontsize=20)
         plt.xlabel('year')
         plt.ylabel(f'{metric} of {y_var}')
-        plt.grid(True)
+        _=plt.grid(True)
         plt.tight_layout()
         plt.legend(title='crop')
         plt.savefig(f"{pathout_plots}{metric}_{y_var}_by_{group_var}.png", bbox_inches='tight', dpi=300)
@@ -237,7 +222,7 @@ def plot_time_series_by_group(df, y_var, pathout_plots, crops, group_var=None, m
         axes[i].set_title(group, position=(0.5, 1.02))
         axes[i].set_xlabel('year')
         axes[i].set_ylabel(f'{metric} {y_var}')
-        axes[i].grid(True)
+        _=axes[i].grid(True)
         axes[i].legend(title='crop')
 
     # remove any remaining empty subplots
@@ -336,6 +321,105 @@ def compute_aggregations(df, variables_to_compute, new_variable_names, metrics, 
     aggregation = aggregation.rename(columns={variable: new_name for variable, new_name in zip(variables_to_compute, new_variable_names)})
 
     return aggregation
+
+
+def plot_global_yield_change(data_df, lower_percentile, upper_percentile, pathout_plots):
+    """
+    Plot yearly change in global median of yield (tonnes/ha) with horizontal lines
+    and highlighted instances within the specified lower and upper percentiles.
+
+    Parameters:
+    - data_df: DataFrame containing the data.
+    - lower_percentile: Lower percentile value (e.g., 0.1 for 10th percentile).
+    - upper_percentile: Upper percentile value (e.g., 0.9 for 90th percentile).
+    - pathout_plots: Output path for saving the plot (without file extension).
+    """
+    # create a dictionary to map each crop to a unique index
+    crop_to_index = {crop: i for i, crop in enumerate(data_df['crop'].unique())}
+    color_map = plt.get_cmap()
+
+    plt.figure(figsize=(12, 6))
+
+    # calculate the lower and upper percentiles for each crop
+    lower_percentiles = data_df.groupby('crop')['global_yield_change'].quantile(lower_percentile).to_dict()
+    upper_percentiles = data_df.groupby('crop')['global_yield_change'].quantile(upper_percentile).to_dict()
+
+    # plotting the main lineplot
+    sns.lineplot(data=data_df, x="year", y="global_yield_change", hue="crop", errorbar=('ci', 95))
+
+    # adding horizontal lines and highlighting instances within the specified percentiles
+    dot_size = 50
+    for crop, percentile in lower_percentiles.items():
+        plt.axhline(percentile, color=color_map(crop_to_index[crop]), linestyle='--', label=f"{crop} lower {int(lower_percentile * 100)}th percentile")
+        below_percentile = data_df[(data_df['crop'] == crop) & (data_df['global_yield_change'] < percentile)]
+        plt.scatter(below_percentile['year'], below_percentile['global_yield_change'], color=color_map(crop_to_index[crop]), zorder=5, s=dot_size, label=f"{crop} below lower {int(lower_percentile * 100)}th percentile")
+
+    for crop, percentile in upper_percentiles.items():
+        plt.axhline(percentile, color=color_map(crop_to_index[crop]), linestyle='-.', label=f"{crop} upper {int(upper_percentile * 100)}th percentile")
+        above_percentile = data_df[(data_df['crop'] == crop) & (data_df['global_yield_change'] > percentile)]
+        plt.scatter(above_percentile['year'], above_percentile['global_yield_change'], color=color_map(crop_to_index[crop]), zorder=5, s=dot_size, marker="^", label=f"{crop} above upper {int(upper_percentile * 100)}th percentile")
+
+    filename = f"{pathout_plots}median_global_yield_change_{int(lower_percentile * 100)}_{int(upper_percentile * 100)}.png"
+    
+    plt.title('yearly change in global median of yield (tonnes/ha)')
+    plt.xlabel('year')
+    plt.ylabel('change in global yield (median tonnes/ha)')
+    plt.legend(title="Legend", loc="upper left", fontsize='small', ncol=2)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(filename, bbox_inches='tight', dpi=300)    
+    plt.show()
+
+
+
+def plot_envelope_global_yield_change(data_df, lower_decile, upper_decile, pathout_plots):
+    """
+    Plot yearly change in global median of yield (tonnes/ha) with horizontal lines
+    and highlighted instances within the specified lower and upper deciles.
+
+    Parameters:
+    - data_df: DataFrame containing the data.
+    - lower_decile: Lower decile value (e.g., 0.1 for 10th decile).
+    - upper_decile: Upper decile value (e.g., 0.9 for 90th decile).
+    - pathout_plots: Output path for saving the plot (without file extension).
+    """
+    plt.figure(figsize=(12, 6))
+
+    # calculate the lower and upper deciles for each crop
+    lower_deciles = data_df.groupby('crop')['global_yield_change'].quantile(lower_decile).to_dict()
+    upper_deciles = data_df.groupby('crop')['global_yield_change'].quantile(upper_decile).to_dict()
+
+    # define colors for each crop
+    colors = {crop: color for crop, color in zip(data_df['crop'].unique(), sns.color_palette())}
+
+    # iterate through each crop
+    for crop, decile in lower_deciles.items():
+        # data points below the lower decile
+        below_decile = data_df[(data_df['crop'] == crop) & (data_df['global_yield_change'] < decile)]
+        
+        # plot data points below the lower decile as a line with markers 'o'
+        plt.plot(below_decile['year'], below_decile['global_yield_change'], label=f"{crop} below {int(lower_decile * 100)}th percentile", marker='o', color=colors[crop])
+
+    for crop, decile in upper_deciles.items():
+        # data points above the upper decile
+        above_decile = data_df[(data_df['crop'] == crop) & (data_df['global_yield_change'] > decile)]
+        
+        # plot data points above the upper decile as a line with markers '^'
+        plt.plot(above_decile['year'], above_decile['global_yield_change'], label=f"{crop} above {int(upper_decile * 100)}th percentile", marker='^', color=colors[crop])
+
+    plt.title(f'upper and lower percentiles in yearly change for wheat & maize')
+    plt.xlabel('year')
+    plt.ylabel('change in global yield (median tonnes/ha)')
+    plt.legend(title="Legend", loc="upper left", fontsize='small', ncol=2)
+    plt.grid(True)
+    plt.tight_layout()
+    
+    # Include lower and upper deciles in the filename
+    filename = f"{pathout_plots}median_global_yield_change_envelope_{int(lower_decile * 100)}_{int(upper_decile * 100)}.png"
+    
+    plt.savefig(filename, bbox_inches='tight', dpi=300)
+    plt.show()
+
 
 
 
